1cbe905ec1a5db823ce1caed2511b03502f92d97
aryangupta701 <garyan447@gmail.com>
2021-10-17 13:12:22 +0530

    colors

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const buttons = document.getElementsByClassName('calculator-buttons')
 const inputbar=document.getElementById('input-tab')
+let flag=0;
 for(let i=0;i<18;i++)
 {
     if(buttons[i].innerText=='Del'){
@@ -19,14 +20,32 @@ for(let i=0;i<18;i++)
     }
     if(buttons[i].innerText=='='){
         buttons[i].addEventListener('click',()=>{
-            console.log(eval(inputbar.value) )
+            inputbar.value=eval(inputbar.value)
+            flag=1; 
         })
        
         continue;
     }
     
     buttons[i].addEventListener('click',()=>{
-        if(inputbar.value.length==1 && inputbar.value[0]=='0')
+        if(buttons[i].innerText=='+' || buttons[i].innerText=='-' || 
+        buttons[i].innerText=='*' || buttons[i].innerText=='/')
+        {
+            if(inputbar.value[inputbar.value.length-1]=='+' ||
+            inputbar.value[inputbar.value.length-1]=='*' ||
+            inputbar.value[inputbar.value.length-1]=='/' ||
+            inputbar.value[inputbar.value.length-1]=='-')
+            inputbar.value=inputbar.value.substr(0,inputbar.value.length-1)
+
+            inputbar.value += buttons[i].innerText
+
+        }
+        else if(flag==1)
+        {
+            inputbar.value = buttons[i].innerText;
+            flag=0;
+        }
+        else if(inputbar.value.length==1 && inputbar.value[0]=='0')
         {
             inputbar.value = buttons[i].innerText;
             
